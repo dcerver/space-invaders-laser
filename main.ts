194cf30a3d393c04c807366c807dd21174f17371
dcerver <121509667+dcerver@users.noreply.github.com>
2023-08-21 12:21:50 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    ship.change(LedSpriteProperty.X, -1)
+	
 })
 input.onButtonPressed(Button.AB, function () {
     music.play(music.createSoundExpression(WaveShape.Square, 1600, 1, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
@@ -11,8 +11,17 @@ input.onButtonPressed(Button.AB, function () {
     Laser.delete()
 })
 input.onButtonPressed(Button.B, function () {
-    ship.change(LedSpriteProperty.X, 1)
+	
 })
 let Laser: game.LedSprite = null
 let ship: game.LedSprite = null
 ship = game.createSprite(2, 4)
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) > 100) {
+        ship.change(LedSpriteProperty.X, 1)
+        basic.pause(750)
+    } else if (input.acceleration(Dimension.X) < -100) {
+        ship.change(LedSpriteProperty.X, -1)
+        basic.pause(750)
+    }
+})
